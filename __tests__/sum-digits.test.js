a56815fb054713ca44904ee0dc6a2137aeff16aa
Daniel Williams <autoboots@gmail.com>
2024-03-11 11:31:35 +0000

    adds tests for sumDigits and writes  function to pass them

--- __tests__/sum-digits.test.js
+++ __tests__/sum-digits.test.js
@@ -10,7 +10,16 @@ const sumDigits = require('../katas/sum-digits');
   E.g. sumDigits(1) should output 1
   E.g. sumDigits(9) should output 9
 */
-
+describe("sumDigits for single digits",()=>{
+  test("single digit should return same digit",()=>{
+    const returnValue = (sumDigits(1));
+    expect(returnValue).toBe(1);
+  });
+  test("a different single digit should return same digit",()=>{
+    const returnValue2 = (sumDigits(9));
+    expect(returnValue2).toBe(9);
+  });
+})
 /*
   Once you have got the first test passing, then you can write your next one.
   If you've already handled a single digit, your next test could test for multi-digit inputs.
@@ -25,6 +34,20 @@ const sumDigits = require('../katas/sum-digits');
   but you don't have to think about the logic for dealing with/ignoring non-digit characters yet
   (that's for our next test!)
 */
+describe("sumDigits for multiple digits",()=>{
+  test("two digit number should return sum of digits",()=>{
+    const returnValue = (sumDigits(99));
+    expect(returnValue).toBe(18);
+  });
+  test("three digit number should return sum of digits",()=>{
+    const returnValue2 = (sumDigits(123));
+    expect(returnValue2).toBe(6);
+  });
+  test("number with many digits should  return sum",()=>{
+    const returnValue2 = (sumDigits(123456789));
+    expect(returnValue2).toBe(45);
+  })
+})
 
 /*
   Once you have successfully passed the above test, then you can write your next test.
@@ -32,3 +55,13 @@ const sumDigits = require('../katas/sum-digits');
   
   E.g. sumDigits(10.5) should output 6
 */
+describe("sumDigits for non-numerical characters and strings",()=>{
+  test("decimal point should be ignored",()=>{
+    const returnValue = (sumDigits(10.5));
+    expect(returnValue).toBe(6);
+  });
+  test("string input should still sum digits",()=>{
+    const returnValue2 = (sumDigits("12.3"));
+    expect(returnValue2).toBe(6);
+  });
+})
--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -1,3 +1,14 @@
-function sumDigits() {}
+function sumDigits(num) {
+
+    let stringOfNum = num.toString();
+    let counter = 0;
+
+    for (let i = 0; i < stringOfNum.length; i++){
+        if(stringOfNum[i].match(/[0-9]/)){
+            counter += Number(stringOfNum[i]);
+        }
+    }
+    return counter;
+}
 
 module.exports = sumDigits;
